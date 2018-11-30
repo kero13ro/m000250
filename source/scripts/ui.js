3385e15bf5e9782b077bf9becae1833019ae6f21
kero <kero13@livemail.tw>
2018-11-30 18:01:28 +0800

    1130 menu RWD index

--- source/scripts/ui.js
+++ source/scripts/ui.js
@@ -6,25 +6,30 @@ var wH = $(window).height();
 var esc = $.Event("keydown", { keyCode: 27 });
 
 
+//scroll fix navbar
+
+function check_nav_fix() {
+  var $nav = $("#nav");
+  // var navH = $nav.height() + $nav.find(".nav__shift").height();
+  var navH = $nav.height();
+  var window_top_position = $window.scrollTop();
+  
+  if (window_top_position > navH) {
+    $nav.addClass("fixed-js");
+  } else {
+    $nav.removeClass("fixed-js");
+  }
+  // $("#navPad").css("height", navH+"px");
+}
+
+$window.on("scroll resize", check_nav_fix);
+
+
+check_nav_fix();
+
+
 $("document").ready(function () {
 
-
-  //scroll fix navbar
-  var $nav = $("#nav");
-  var navH = $nav.height() + $nav.find(".nav__shift").height();
-
-  function check_nav_fix() {
-    var window_top_position = $window.scrollTop();
-    
-    if (window_top_position > navH) {
-      $nav.addClass("fixed-js");
-    } else {
-      $nav.removeClass("fixed-js");
-    }
-  }
-
-  $window.on("scroll resize", check_nav_fix);
-  check_nav_fix();
 
 });
 
